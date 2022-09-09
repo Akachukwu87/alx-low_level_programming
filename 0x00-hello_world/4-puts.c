1a62a75fd9fc478a8593598bb4ba18e6ecc1c897
Akachukwu87 <tulk2icecocaf47@yahoo.com>
2022-09-09 16:02:11 -0700

    myfiles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *min-printing
- *
- * Return: Always 0 (Successful)
- */
- int main (void)
-  {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
-  }
+*min-printing
+*
+* Return: Always 0 (Successful)
+*/
+int main (void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
        
 /**
- *main - Prints a text 
- *
- * Return:Always 0 (Success)
- */
+*main - Prints a text 
+*
+* Return:Always 0 (Success)
+*/
 int main (void)
- {
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
- return (0);
- }
+return (0);
+}
