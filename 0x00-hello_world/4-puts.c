12f12df0a890cc96f8607539a4e01670d053bd81
Akachukwu87 <tulk2icecocaf47@yahoo.com>
2022-09-09 16:44:51 -0700

    my files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*min-printing
+*main-printing
 *
-* Return: Always 0 (Successful)
+*Return:Always 0(Successful)
 */
-int main (void) 
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
