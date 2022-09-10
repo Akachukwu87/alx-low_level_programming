be837b74112d30dbd65bf983cf3833e9f5d6faf7
Akachukwu87 <tulk2icecocaf47@yahoo.com>
2022-09-10 01:41:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 *main-printing
 *
-*Return:Always0(Successful)
+*Return:Always 0 ( Successful )
 */
-int main(void) 
+int main ( void ) 
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+puts ( "\"Programming is like building a multilingual puzzle" );
+return (0);
 }
