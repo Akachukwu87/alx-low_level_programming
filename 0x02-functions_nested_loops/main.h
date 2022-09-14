4f7bfe1292f56f08a1f880b79851e176f96a0931
Akachukwu87 <tulk2icecocaf47@yahoo.com>
2022-09-14 06:26:23 -0700

    file2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-int _putchar(char);
 void print_alphabet(void);
-void print _alphabet_10x(void);
-int _islower(int c);
+print_alphabet_x10(void);
+int islower(int c);
+int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_time_table(int n);
 
 #endif
