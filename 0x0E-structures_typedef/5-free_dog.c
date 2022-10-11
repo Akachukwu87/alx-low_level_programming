b6b8225089b96f92bd20dd46ae981dbe7e6c6648
Akachukwu87 <tulk2icecocaf47@yahoo.com>
2022-10-10 20:37:03 -0700

    files

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+[m
